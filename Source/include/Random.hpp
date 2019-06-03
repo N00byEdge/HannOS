7a8bf21dc5d87e51d746661ac065b91748973cc6
N00byEdge <me@hannesbredberg.se>
2019-06-04 01:19:15 +0200

    Update Random.hpp for 64 bit

--- Source/include/Random.hpp
+++ Source/include/Random.hpp
@@ -4,18 +4,18 @@
 
 namespace HannOS::CPU {
   struct Random {
-    using result_type = std::uint32_t;
-    static constexpr result_type min() { return std::numeric_limits<std::uint32_t>::min(); }
-    static constexpr result_type max() { return std::numeric_limits<std::uint32_t>::max(); }
+    using result_type = std::uint64_t;
+    static constexpr result_type min() { return std::numeric_limits<std::uint64_t>::min(); }
+    static constexpr result_type max() { return std::numeric_limits<std::uint64_t>::max(); }
 
-    template<typename T = std::uint32_t>
+    template<typename T = std::uint64_t>
     static auto get() {
       T retval;
       asm("rdrand %0":"=r"(retval));
       return retval;
     }
 
-    int operator()() {
+    auto operator()() {
       return get();
     }
   };
